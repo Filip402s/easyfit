c9fcff053b9508e064d60913a956b4f39010247f
mazakk94 <mazakk94@gmail.com>
2021-02-11 22:50:21 +0100

    [65]: cleanup sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,14 @@
 echo '*** deleting containers'
 docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+
 echo '*** deleting volumes'
 docker volume rm $(docker volume ls -qf dangling=true)
-echo 'deleting images'
+
+echo '*** deleting images'
 docker rmi $(docker images -q -a)
-echo 'restarting docker'
-service docker restart
+
+echo '*** restarting runner'
+docker restart gitlab-runner
+
+# echo '*** restarting docker'
+# service docker restart
